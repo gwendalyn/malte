4573052a690f2771160ed8122e5f573da4ab729a
glenlivet <kikyou1986@hotmail.com>
2014-02-22 23:11:26 +0800

    rontai

---
+++ rontai-p-rontai/src/main/java/org/rontai/p/rontai/App.java
@@ -0,0 +1,13 @@
+package org.rontai.p.rontai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
