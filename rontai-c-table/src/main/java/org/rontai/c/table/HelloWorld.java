fee7c4c89bcd0becb14d5df19e79be7242bd0518
allan2k1987 <allan2k@qq.com>
2014-03-05 20:35:31 +0800

    Revert "Revert "rontai""
    This reverts commit f96f3b16af46b388209de77ebf661d4669091723.

---
+++ rontai-c-table/src/main/java/org/rontai/c/table/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.rontai.c.table;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class HelloWorld {
+	
+	public void shout(){
+		System.out.println("hello world");
+	}
+
+}
---
+++ rontai-c-table/src/main/java/org/rontai/c/table/TableBean.java
@@ -0,0 +1,40 @@
+package org.rontai.c.table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import org.rontai.s.base.SpringContext;
+
+
+@ManagedBean
+@SessionScoped
+public class TableBean {
+	
+	private List<Car> carsSmall;
+	
+	public TableBean(){
+		HelloWorld hw = (HelloWorld)SpringContext.context.getBean("helloWorld");
+		hw.shout();
+	}
+	
+	
+	public List<Car> getCarsSmall() {
+		
+		Car c1 = new Car("model1", 1881, "manu1", "yellow");
+		Car c2 = new Car("model2", 1892, "manu2", "red");
+		carsSmall = new ArrayList<Car>();
+		carsSmall.add(c1);
+		carsSmall.add(c2);
+		return carsSmall;
+	}
+
+	public void setCarsSmall(List<Car> carsSmall) {
+		this.carsSmall = carsSmall;
+	}
+	
+	
+
+}
