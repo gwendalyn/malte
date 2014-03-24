7c66eda25d3d0735f8a4cdf806ae1726836c036c
Zhang, Shulai <Shulai.Zhang@sungard.com>
2014-03-24 18:55:36 +0800

    logging

--- rontai-p-template/src/main/java/org/rontai/p/template/App.java
+++ rontai-p-template/src/main/java/org/rontai/p/template/App.java
@@ -1,11 +1,15 @@
 package org.rontai.p.template;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static Logger logger = Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
