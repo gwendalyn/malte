bc627bff620d06f8186b5210240112ba447302cc
glenlivet <kikyou1986@hotmail.com>
2014-03-30 20:22:19 +0800

    cc

--- rontai-p-template/src/main/java/org/rontai/p/log/LogTest.java
+++ rontai-p-template/src/main/java/org/rontai/p/log/LogTest.java
@@ -8,14 +8,18 @@ public class LogTest {
 	static Logger logger = LoggerFactory.getLogger(LogTest.class);
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++){
-			logger.info("from log package");
-			try {
-				Thread.sleep(30000);
-			} catch (InterruptedException e) {
-				
-			}
+		try {
+			doSomething();
+		} catch (Exception e) {
+			logger.trace("cc", e);
 		}
+		logger.debug("111");
+		logger.info("info");
+		logger.warn("warn");
+	}
+
+	private static void doSomething() {
+		throw new RuntimeException("abc");
 	}
 
 }
